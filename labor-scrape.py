37d76d007a143de41d93d6c424ca505006f2adba
Suhailhassanbhat <SuhailBhat@Kazis-MBP.lan1>
2023-05-22 11:50:35 -0400

    restructure

--- labor-scrape.py
+++ labor-scrape.py
@@ -5,7 +5,11 @@
 
 
 import requests
+import warnings
 import pandas as pd
+# Ignore all warnings
+warnings.filterwarnings("ignore")
+
 import numpy as np
 from urllib.request import urlopen, Request
 from urllib.parse import urlencode
@@ -259,7 +263,7 @@ trans['release_date']=trans.release_date.replace({"4/21/203":'4/21/2023'})
 trans['release_date']=pd.to_datetime(trans.release_date)
 trans=trans[~trans.name.str.contains("NSA")]
 sorted_trans=trans.sort_values('release_date', ascending=False)
-sorted_trans
+
 # download excel file from the link in url
 url=sorted_trans.URL[0]
 baseURL='https://www1.nyc.gov'
@@ -285,7 +289,9 @@ filtered_df=filtered_df[['month', 'Total Nonfarm', 'Total Private', 'Financial A
     'Wholesale Trade', 'Transportation and Warehousing', 'Utilities','Construction', ' Manufacturing', ' Government']]
 
 filtered_df['datetime']=pd.to_datetime(filtered_df.month)
-filtered_df=filtered_df.sort_values('datetime', ascending=False)
+filtered_df=filtered_df.sort_values('datetime', ascending=False).reset_index(drop=True)
+
+latest_month=pd.to_datetime(filtered_df.month).dt.strftime("%m")[0]
 
 filtered_df=filtered_df.drop(columns='datetime')
 
@@ -293,12 +299,13 @@ transposed=filtered_df.set_index('month').T.reset_index().rename(columns={"index
 
 transposed['sector']=transposed.sector.str.strip()
 
-transposed['yoy_chng']=(((transposed.iloc[:, 1]-transposed.iloc[:, 13])/transposed.iloc[:, 13])*100).round(1)
-transposed['2y_chng']=(((transposed.iloc[:, 1]-transposed.iloc[:, 25])/transposed.iloc[:, 25])*100).round(1)
-transposed['3y_chng']=(((transposed.iloc[:, 1]-transposed.iloc[:, 37])/transposed.iloc[:, 37])*100).round(1)
+column_names=transposed.columns[
+    transposed.columns.str.contains(f'sector|2019-{latest_month}|2023-{latest_month}')].to_list()
 
-# final_df=transposed[['sector','yoy_chng', '2y_chng', '3y_chng']]
-final_df=transposed.iloc[:,[0,1, -1, -2, -3]].sort_values('3y_chng', ascending=False).reset_index(drop=True)
+transposed=transposed[column_names]
+
+transposed['chngFromPrepandmic']=(
+    (transposed.iloc[:, 1]-transposed.iloc[:, -1]).div(transposed.iloc[:, -1])*100).round(1)
 
 sectors=['Finance and Insurance', 'Real Estate','Information', 'Professional, Scientific, and Technical Services',
          'Management of Companies and Enterprises', 'Administrative Services', 'Employment Services',
@@ -306,90 +313,9 @@ sectors=['Finance and Insurance', 'Real Estate','Information', 'Professional, Sc
          'Accommodation and Food Services', 'Retail Trade','Wholesale Trade', 'Transportation and Warehousing',
          'Utilities','Construction', 'Manufacturing', 'Government']
 
-cleaned_df=final_df[final_df.sector.isin(sectors)].reset_index(drop=True)
+cleaned_df=transposed[transposed.sector.isin(sectors)].reset_index(drop=True)
 
 cleaned_df['month']=cleaned_df.columns[1]
-cleaned_df=cleaned_df.rename(columns={cleaned_df.columns[1]:'jobs'})
 
 cleaned_df.to_json('data/industry.json', orient='records')
-
-
-# # Office and Hotel Occupancy
-
-# In[9]:
-
-
-sheet_id = "1JLlcLJ_dKBct7zK804L-p7P_7SYlIRegBHSfIKs8ek0"
-sheet_name = "office_occupancy"
-officeURL = f'https://docs.google.com/spreadsheets/d/{sheet_id}/gviz/tq?tqx=out:csv&sheet={sheet_name}'
-
-oc=pd.read_csv(officeURL) 
-oc=oc.replace("%", "", regex=True)
-oc=oc.melt(id_vars='week_ending').rename(columns={'variable':'metro', 'value':'occupancy'})
-oc.to_json('data/occupancy.json', orient='records')
-
-
-# In[10]:
-
-
-sheet_name = "hotel_occupancy"
-hotelURL = f'https://docs.google.com/spreadsheets/d/{sheet_id}/gviz/tq?tqx=out:csv&sheet={sheet_name}'
-
-hotel=pd.read_csv(hotelURL) 
-demand=hotel[['period', 'demand2020', 'demand2021', 'demand2022']]
-change=hotel[['period', 'change2020', 'change2021', 'change2022']]
-
-demand.columns=demand.columns.str.replace('demand',"")
-change.columns=demand.columns.str.replace('change',"")
-
-melted_demand=demand.melt(id_vars='period').dropna().rename(
-    columns={'value':'demand', 'variable':'year'}).reset_index(drop=True)
-melted_change=change.melt(id_vars='period').dropna().rename(
-    columns={'value':'pct_chng', 'variable':'year'}).reset_index(drop=True)
-merged_hotel=pd.merge(melted_demand, melted_change, on=['period', 'year'])
-
-merged_hotel['month']=pd.to_datetime(merged_hotel.period+"-"+merged_hotel.year).dt.strftime("%Y-%m")
-final_hotel=merged_hotel[['month', 'demand', 'pct_chng']].reset_index(drop=True)
-
-final_hotel.to_json('data/hotel_demand.json', orient='records')
-
-
-# # Subway ridership
-
-# In[11]:
-
-
-# try: 
-#     !wget -O raw_data/subway.csv "https://data.ny.gov/api/views/vxuj-8kew/rows.csv?accessType=DOWNLOAD&sorting=true"
-# except:
-#     pass
-#     print("No subway file")
-
-
-# In[6]:
-
-
-riders_df=pd.read_json("https://data.ny.gov/api/id/vxuj-8kew.json?$query=select%20*%2C%20%3Aid%20limit%2010000")
-
-riders_df=riders_df[['date', 'subways_total_estimated_ridership', 'subways_of_comparable_pre_pandemic_day']]
-
-
-riders_df.columns=['date', 'riders', 'riders_recovered']
-
-riders_df['date']=pd.to_datetime(riders_df.date)
-
-riders_df=riders_df.sort_values("date")
-
-riders_df['avg_recovery']=(riders_df.riders_recovered.rolling(window=7).mean()*100).round()
-riders_df['date']=pd.to_datetime(riders_df.date).dt.strftime("%Y-%m-%d")
-riders_df=riders_df[6:].reset_index(drop=True)
-
-riders_df=riders_df.dropna(subset=['riders'])
-riders_df.to_json('data/subway_riders.json', orient='records')
-
-
-# In[ ]:
-
-
-
 
---
+++ subway.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import requests
+import warnings
+import pandas as pd
+# Ignore all warnings
+warnings.filterwarnings("ignore")
+
+import numpy as np
+from urllib.request import urlopen, Request
+from urllib.parse import urlencode
+# from selenium import webdriver
+from bs4 import BeautifulSoup
+
+
+# In[3]:
+
+
+## Subway ridership
+
+# try: 
+#     !wget -O raw_data/subway.csv "https://data.ny.gov/api/views/vxuj-8kew/rows.csv?accessType=DOWNLOAD&sorting=true"
+# except:
+#     pass
+#     print("No subway file")
+
+riders_df=pd.read_json("https://data.ny.gov/api/id/vxuj-8kew.json?$query=select%20*%2C%20%3Aid%20limit%2010000")
+
+riders_df=riders_df[['date', 'subways_total_estimated_ridership', 'subways_of_comparable_pre_pandemic_day']]
+
+
+riders_df.columns=['date', 'riders', 'riders_recovered']
+
+riders_df['date']=pd.to_datetime(riders_df.date)
+
+riders_df=riders_df.sort_values("date")
+
+riders_df['avg_recovery']=(riders_df.riders_recovered.rolling(window=7).mean()*100).round()
+riders_df['date']=pd.to_datetime(riders_df.date).dt.strftime("%Y-%m-%d")
+riders_df=riders_df[6:].reset_index(drop=True)
+
+riders_df=riders_df.dropna(subset=['riders'])
+riders_df.to_json('data/subway_riders.json', orient='records')
+
+
+# OFFICE OCCUPANCY
+sheet_id = "1JLlcLJ_dKBct7zK804L-p7P_7SYlIRegBHSfIKs8ek0"
+sheet_name = "office_occupancy"
+officeURL = f'https://docs.google.com/spreadsheets/d/{sheet_id}/gviz/tq?tqx=out:csv&sheet={sheet_name}'
+
+oc=pd.read_csv(officeURL) 
+oc=oc.replace("%", "", regex=True)
+oc=oc.melt(id_vars='week_ending').rename(columns={'variable':'metro', 'value':'occupancy'})
+oc.to_json('data/occupancy.json', orient='records')
+
+
+## HOTEL OCCUPANCY
+sheet_name = "hotel_occupancy"
+hotelURL = f'https://docs.google.com/spreadsheets/d/{sheet_id}/gviz/tq?tqx=out:csv&sheet={sheet_name}'
+
+hotel=pd.read_csv(hotelURL) 
+demand=hotel[['period', 'demand2020', 'demand2021', 'demand2022']]
+change=hotel[['period', 'change2020', 'change2021', 'change2022']]
+
+demand.columns=demand.columns.str.replace('demand',"")
+change.columns=demand.columns.str.replace('change',"")
+
+melted_demand=demand.melt(id_vars='period').dropna().rename(
+    columns={'value':'demand', 'variable':'year'}).reset_index(drop=True)
+melted_change=change.melt(id_vars='period').dropna().rename(
+    columns={'value':'pct_chng', 'variable':'year'}).reset_index(drop=True)
+merged_hotel=pd.merge(melted_demand, melted_change, on=['period', 'year'])
+
+merged_hotel['month']=pd.to_datetime(merged_hotel.period+"-"+merged_hotel.year).dt.strftime("%Y-%m")
+final_hotel=merged_hotel[['month', 'demand', 'pct_chng']].reset_index(drop=True)
+
+final_hotel.to_json('data/hotel_demand.json', orient='records')
+
+
+# In[ ]:
+
+
+
+
